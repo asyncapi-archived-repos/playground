27f8c72e276b67097ae195f0cce43360ecf128fb
Lukasz Gornicki <lpgornicki@gmail.com>
2020-12-14 11:27:20 +0100

    chore: switch to new converter under asyncapi scope (#51)

--- src/server/routes/convert.js
+++ src/server/routes/convert.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { convert } = require('asyncapi-converter');
+const { convert } = require('@asyncapi/converter');
 
 module.exports = router;
 
